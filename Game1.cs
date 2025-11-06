cf1ac655f1dfc0a6cb348274873a2ac91b0c5c87
339713067 <339713067@2260I-L29-023>
2025-11-06 09:39:53 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace topic_2_mono_game
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        KeyboardState keyboardstate;
+        Texture2D mowertexture;
+        Texture2D longgrasstexture;
+
+        Rectangle window;
+        Vector2 mowerspeed;
+
+        Rectangle mowerrectangle;
+        SoundEffect mowerSound;
+        SoundEffectInstance mowerSoundInstance;
+
+        List<Rectangle> grassTiles;
+        
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0, 0, 600, 500);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            mowerrectangle = new Rectangle(100, 100, 30, 30);
+
+            grassTiles = new List<Rectangle>();
+            for (int x = 0; x < window.Width; x += 5)
+                for (int y = 0; y < window.Height; y += 5)
+                    grassTiles.Add(new Rectangle(x, y, 5, 5));
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            mowertexture = Content.Load<Texture2D>("images/mower");
+            longgrasstexture = Content.Load<Texture2D>("images/long_grass");
+            mowerSound = Content.Load<SoundEffect>("sounds/mower_sound");
+            mowerSoundInstance = mowerSound.CreateInstance();
+            mowerSoundInstance.IsLooped = true;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            keyboardstate = Keyboard.GetState();
+            mowerspeed = Vector2.Zero;
+            if (keyboardstate.IsKeyDown(Keys.W))
+            {
+                mowerspeed.Y += -4;
+            }
+            if (keyboardstate.IsKeyDown(Keys.A))
+            {
+                mowerspeed.X += -4;
+            }
+            if (keyboardstate.IsKeyDown(Keys.S))
+            {
+                mowerspeed. Y += 4;
+            }
+            if (keyboardstate.IsKeyDown(Keys.D))
+            {
+                mowerspeed.X += 4dsss;
+            }
+            mowerrectangle.X += (int)mowerspeed.X;
+            mowerrectangle.Y += (int)mowerspeed.Y;
+            if (mowerspeed != Vector2.Zero)
+            {
+                if (mowerSoundInstance.State != SoundState.Playing)
+                {
+                    mowerSoundInstance.Play();
+                }
+            }
+            else
+            {
+                mowerSoundInstance.Pause();
+            }
+            for (int i = 0; i < grassTiles.Count; i++)
+                if (mowerrectangle.Contains(grassTiles[i]))
+                {
+                    grassTiles.RemoveAt(i);
+                    i--;
+
+                }
+
+
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            foreach(Rectangle grass in grassTiles)
+            {
+                _spriteBatch.Draw(longgrasstexture, grass, Color.White);
+            }
+           
+            _spriteBatch.Draw(mowertexture, mowerrectangle, Color.White);
+
+
+
+
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new topic_2_mono_game.Game1();
+game.Run();
